ea7fd686c8541a75fa56d49f8648c855d26d44e4
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-17 20:47:45 -0700

    Create basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
-<template>hello, world!</template>
+<template>
+  <div class="dvh-15">
+    <div>Header</div>
+  </div>
+  <div class="min-dvh-75">
+    <div>Main</div>
+  </div>
+  <div class="dvh-10">
+    <div>Footer</div>
+  </div>
+</template>
