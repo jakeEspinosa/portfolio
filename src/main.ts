99ea92be40a4a1742c8e33ee337c89b1e54a1225
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-17 20:24:53 -0700

    Install bootstrap.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 
+import "./assets/scss/styles.scss";
+import * as bootstrap from "bootstrap";
+
 const app = createApp(App);
 
 app.mount("#app");
