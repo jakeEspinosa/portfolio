e545f7e5d39893d37edd015b55ac4e844aa02e90
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-18 21:08:38 -0700

    Add main content area.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ See LICENSE.txt for details.
 
 <script setup lang="ts">
   import FooterComponent from "@/components/main-view/footer/FooterComponent.vue";
+  import MainContent from "@/components/main-view/MainContent.vue";
   import constants from "@/utilities/constants";
 </script>
 
@@ -12,8 +13,8 @@ See LICENSE.txt for details.
   <header class="dvh-15">
     <div>Header</div>
   </header>
-  <main class="min-dvh-75">
-    <div>Main</div>
+  <main>
+    <MainContent />
   </main>
   <footer class="dvh-10">
     <FooterComponent :copyright-string="constants.copyrightString" />
---
+++ src/components/main-view/MainContent.vue
@@ -0,0 +1,8 @@
+<!-- 
+Copyright (C) 2023 Jake Espinosa Under the MIT License
+See LICENSE.txt for details.
+-->
+
+<template>
+  <div class="secondary-bg min-dvh-75"></div>
+</template>
