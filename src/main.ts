ec0333cd8d5ee2328ac3c9cd4c1b7dff7563c070
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-18 20:32:37 -0700

    Absolute path.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 
-import "./assets/scss/styles.scss";
+import "@/assets/scss/styles.scss";
 import * as bootstrap from "bootstrap";
 
 const app = createApp(App);
