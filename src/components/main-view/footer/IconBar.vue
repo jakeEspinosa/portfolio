3670d4bc2b357bb59b1a4f8a1f65e715751969fe
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-18 20:37:08 -0700

    Install icons and add icon bar.

---
+++ src/components/main-view/footer/IconBar.vue
@@ -0,0 +1,15 @@
+<!-- 
+Copyright (C) 2023 Jake Espinosa Under the MIT License
+See LICENSE.txt for details.
+ -->
+
+<template>
+  <div class="container-fluid d-flex justify-content-center gap-3">
+    <a href="https://www.linkedin.com/in/jake-espinosa/" target="_blank"
+      ><i class="bi bi-linkedin link-opacity-50-hover"
+    /></a>
+    <a href="https://github.com/jakeEspinosa" target="_blank"
+      ><i class="bi bi-github link-opacity-50-hover"
+    /></a>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 
 import "@/assets/scss/styles.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import * as bootstrap from "bootstrap";
 
 const app = createApp(App);
