30f7edf4bcc9cf47ad7567f3e37c561d26c96a38
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-23 08:02:18 -0700

    Revert "Copy/paste navbar."
    This reverts commit dde2405354317f246afd1c8ca39f624035aa911f.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import router from "@/router";
 import App from "@/App.vue";
 
 import "@/assets/scss/styles.scss";
@@ -7,5 +6,5 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import * as bootstrap from "bootstrap";
 
 const app = createApp(App);
-app.use(router);
+
 app.mount("#app");
