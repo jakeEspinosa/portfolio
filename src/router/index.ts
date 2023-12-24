f1226df9b6f324784dfda91ca49334a3ea8ad4ae
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-12-23 21:23:28 -0700

    Build landing page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,15 @@ See LICENSE.txt for details.
 */
 
 import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "@/views/LandingPage.vue";
 import AboutPage from "@/views/AboutPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/about", component: AboutPage }],
+  routes: [
+    { path: "/", component: LandingPage },
+    { path: "/about", component: AboutPage },
+  ],
 });
 
 export default router;
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,11 @@
+<!--
+Copyright (C) 2023 Jake Espinosa Under the MIT License
+See LICENSE.txt for details.
+-->
+
+<template>
+  <h2 class="main-text pt-3">Full Stack Software Engineer</h2>
+  <p class="main-text fs-5">
+    I help organizations develop accessible, secure software.
+  </p>
+</template>
