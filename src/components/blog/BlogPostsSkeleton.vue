c9a46eee35a200df9a9734736c9b7797096c7ef7
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-01-23 10:36:09 -0700

    center

--- src/components/blog/BlogPostsSkeleton.vue
+++ src/components/blog/BlogPostsSkeleton.vue
@@ -4,7 +4,10 @@ See LICENSE.txt for details.
 -->
 
 <template>
-  <h3 aria-hidden="true" class="placeholder-glow fs-2 pb-2">
+  <h3
+    aria-hidden="true"
+    class="placeholder-glow fs-2 pb-2 container-fluid d-flex justify-content-center"
+  >
     <span class="placeholder col-6 main-text"></span>
   </h3>
 </template>
