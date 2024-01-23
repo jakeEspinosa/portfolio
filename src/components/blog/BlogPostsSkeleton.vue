68b8d05280ad3f9346228cebaf69e556a1d95ec2
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-01-23 10:22:20 -0700

    Remove unnecessary lines

--- src/components/blog/BlogPostsSkeleton.vue
+++ src/components/blog/BlogPostsSkeleton.vue
@@ -7,10 +7,4 @@ See LICENSE.txt for details.
   <h3 aria-hidden="true" class="placeholder-glow fs-2 pb-2">
     <span class="placeholder col-6 main-text"></span>
   </h3>
-  <p aria-hidden="true" class="placeholder-glow pb-1 mb-1">
-    <span class="col-10 main-text fs-3 placeholder"></span>
-  </p>
-  <p aria-hidden="true" class="placeholder-glow">
-    <span class="col-10 main-text fs-3 placeholder"></span>
-  </p>
 </template>
