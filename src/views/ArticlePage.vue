2c086f996494767f253e5ba99aaf8abd316c3044
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-01-25 21:19:22 -0700

    Render content.

--- src/views/ArticlePage.vue
+++ src/views/ArticlePage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { useRoute } from "vue-router";
-  import { ref } from "vue";
+  import { ref, watch } from "vue";
+  import markdownit from "markdown-it";
   import api from "@/utilities/api";
   import constants from "@/utilities/constants";
 
@@ -8,6 +9,8 @@
   const isLoaded = ref(false);
   const post = ref();
   const URL = constants.blogPostUrl + `?id=${route.params.id}`;
+  const md = markdownit();
+  const content = ref();
 
   const getPosts = async (url: string) => {
     const data = await api.get(url);
@@ -16,8 +19,12 @@
   };
 
   getPosts(URL);
+  watch(post, (newPost) => {
+    content.value = md.render(newPost.content);
+  });
 </script>
 
 <template>
-  {{ post }}
+  <div v-html="post.title"></div>
+  <div v-html="content"></div>
 </template>
