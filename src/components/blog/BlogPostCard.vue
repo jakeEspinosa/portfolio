d24229a46ec20a8d15db6fc76a34897ca52d1e3e
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-01-26 08:11:26 -0700

    Make article links work

--- src/components/blog/BlogPostCard.vue
+++ src/components/blog/BlogPostCard.vue
@@ -4,6 +4,9 @@ See LICENSE.txt for details.
 -->
 
 <script setup lang="ts">
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
   const props = defineProps({
     title: {
       type: String,
@@ -12,12 +15,18 @@ See LICENSE.txt for details.
       type: Number,
     },
   });
+
+  const clickHandler = () => {
+    router.push(`/blog/${props.id}`);
+  };
 </script>
 
 <template>
   <div class="container-fluid d-flex justify-content-center">
-    <h3 class="main-text fs-2 pb-2">
-      {{ props.title }}
-    </h3>
+    <button type="button" class="btn" @click="clickHandler">
+      <h3 class="main-text fs-2 pb-2">
+        {{ props.title }}
+      </h3>
+    </button>
   </div>
 </template>
