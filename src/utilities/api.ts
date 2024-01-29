bf301353ea7a6c417bac5985f02fe4c5ff46d041
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-01-29 14:45:48 -0700

    Document get method.

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -3,6 +3,12 @@ Copyright (C) 2024 Jake Espinosa Under the MIT License
 See LICENSE.txt for details.
 */
 
+/**
+ * @function get
+ * @description performs a GET request to the provided URL. This function works with
+ * query parameters or path parameters.
+ * @param {string} url
+ */
 export default {
   async get(url: string) {
     const response = await fetch(url);
