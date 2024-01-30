c4435d471d7bfa699603d38f9955bfaba1998aa1
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-01-30 07:57:43 -0700

    Format contact page.

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -5,22 +5,22 @@ See LICENSE.txt for details.
 
 <template>
   <h2 class="main-text pt-3">Contact Me!</h2>
-  <h3 class="main-text pt-3">
+  <p class="main-text pt-3 fs-1">
     Email:
     <a
       href="mailto: jacob.sa.espinosa@gmail.com"
-      class="link-opacity-50-hover link-underline-secondary link-underline-opacity-50-hover link-underline-opacity-0"
+      class="link-opacity-50-hover link-underline-secondary link-underline-opacity-50-hover link-underline-opacity-0 d-inline text-break"
       >jacob.sa.espinosa@gmail.com</a
     >
-  </h3>
-  <h3>
+  </p>
+  <p>
     <a
       href="https://www.linkedin.com/in/jake-espinosa/"
       target="_blank"
-      class="link-opacity-50-hover link-underline-secondary link-underline-opacity-50-hover link-underline-opacity-0"
+      class="pt-3 link-opacity-50-hover link-underline-secondary link-underline-opacity-50-hover link-underline-opacity-0 fs-1 text-break"
       >Linkedin</a
     >
-  </h3>
+  </p>
   <p class="main-text fs-5">
     Feel free to reach out to me to chat, to connect on Linkedin, for advice
     (although it may not be the best), or anything else!
