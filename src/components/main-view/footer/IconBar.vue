0acd300165063f69a2ae242944bc2bc7e371f6af
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-02-26 22:11:22 -0700

    Make footer accessible.

--- src/components/main-view/footer/IconBar.vue
+++ src/components/main-view/footer/IconBar.vue
@@ -5,10 +5,16 @@ See LICENSE.txt for details.
 
 <template>
   <div class="container-fluid d-flex justify-content-center gap-3 w-80vw pt-3">
-    <a href="https://www.linkedin.com/in/jake-espinosa/" target="_blank"
+    <a
+      href="https://www.linkedin.com/in/jake-espinosa/"
+      target="_blank"
+      aria-label="linkedin"
       ><i class="bi bi-linkedin link-opacity-50-hover"
     /></a>
-    <a href="https://github.com/jakeEspinosa" target="_blank"
+    <a
+      href="https://github.com/jakeEspinosa"
+      target="_blank"
+      aria-label="github"
       ><i class="bi bi-github link-opacity-50-hover"
     /></a>
   </div>
